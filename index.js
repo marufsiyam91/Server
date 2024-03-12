3de97a4a7147e72bc0d1c1a0e937e0f8d23082fd
Maruf Siyam <marufsiyam9123@gmail.com>
2024-03-12 10:42:20 +0600

    server created

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const mongoose = require('mongoose');
 const express = require('express')
 const cors = require('cors')
+
 const app = express()
 const port = process.env.PORT || 5000
 require('dotenv').config()
@@ -8,8 +10,10 @@ require('dotenv').config()
 app.use(express.json())
 app.use(cors())
 
-const uri = `mongodb+srv://${process.env.HOTEL_USER}:${process.env.HOTEL_PASS}@cluster0.ag0e44c.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
+
+const uri = `mongodb+srv://marufsiyam9123:${process.env.HOTEL_PASS}@hotelbooking.kxuruz8.mongodb.net/?retryWrites=true&w=majority&appName=HotelBooking`
+;
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
@@ -23,12 +27,29 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const HotelCollection = client.db('Hotel_Server').collection('hotels')
+
+    app.get('/hotels', async(req, res) => {
+      const cursor = HotelCollection.find()
+      const result = await cursor.toArray()
+      res.send(result)
+    })
+
+    app.get('/hotels/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)};
+      const result = await HotelCollection.findOne(query)
+      res.send(result)
+    });
     
+    
+
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
